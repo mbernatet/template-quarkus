795657ed6070a07fe22db911b55b998a4c9ac04f
MATHIEU BERNATET <mathieu.bernatet@atos.net>
2023-06-15 09:52:19 +0200

    only push if build is successful

--- src/test/java/net/atos/labbordeaux/GreetingResourceIT.java
+++ src/test/java/net/atos/labbordeaux/GreetingResourceIT.java
@@ -1,8 +1,15 @@
 package net.atos.labbordeaux;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
+
+
+    @Test
+    public void testFailed() {
+        throw new RuntimeException("mspldfkmsdlfk");
+    }
 }
