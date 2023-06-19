9f150eb432a4406135ffdb13dd9a6a496cf14254
MATHIEU BERNATET <mathieu.bernatet@atos.net>
2023-06-19 17:35:37 +0200

    test codeql

--- src/main/java/net/atos/labbordeaux/GreetingResource.java
+++ src/main/java/net/atos/labbordeaux/GreetingResource.java
@@ -4,13 +4,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private static DateFormat format = SimpleDateFormat.getInstance();
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        System.out.println(format.format(new Date()));
+        // throw new RuntimeException("msldkfmsdfl");
         return "Hello from RESTEasy Reactive";
     }
 }
